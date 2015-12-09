d990b89e418d1780dccc33c6371a528ccce06c7a
David Edler <edlerd@gmail.com>
2015-12-09 16:50:29 +0100

    sampler.c add some comments

--- c/sampler.c
+++ c/sampler.c
@@ -5,8 +5,6 @@
 #define BUFSIZE 256
 
 int main(int argc, char *argv[]) {
-  unsigned long long int frequency[256];
-  unsigned long long int frequencyTotal = 0;
 
   char msg[BUFSIZE];
   char character;
@@ -14,6 +12,10 @@ int main(int argc, char *argv[]) {
   int i;
   int charValue;
   int sampleLength=5;
+  
+  // we will count all ascii characters, so 256 values max
+  unsigned long long int frequency[256];
+  unsigned long long int frequencyTotal = 0;
 
   for (i=0; i<256; i++) {
     frequency[i] = 0;
@@ -27,9 +29,13 @@ int main(int argc, char *argv[]) {
     msg[BUFSIZE - 1] = '\0';
     msgLen = strlen(msg);
     for (i=0; i<BUFSIZE-1; i++) {
+
+      // end of stream
       if (msg[i] == '\0') {
         break;
       }
+
+      // update frequency array
       charValue = msg[i];
       if (charValue > 256) {
         printf("skipping non ascii character: %d \n", charValue);
@@ -38,19 +44,15 @@ int main(int argc, char *argv[]) {
       frequency[charValue]++;
       frequencyTotal++;
     }
+    
+    // the buffer is not filled? we must be finished
     if (msgLen < (BUFSIZE - 1))
       break;
   }
-/**
-  for(i=0; i<256; i++) {
-    character = i;
-    if (frequency[i] > 0) {
-      printf("%c has value %llu \n", character, frequency[i]);
-    }
-  }
-/**/
 
+  // init msg to hold the resulting sample
   memset (msg, '\0', BUFSIZE);
+  
   srand(time(NULL));
   for (i=0; i<sampleLength; i++) {
 
@@ -61,8 +63,8 @@ int main(int argc, char *argv[]) {
 	diceRoll += rand();
     }
     diceRoll %= frequencyTotal;
-    printf("diceRoll %llu frequtotal %llu \n", diceRoll, frequencyTotal);
 
+    // find the character the diceRoll represents in the frequency array
     int runner = 0;
     for (k=0; k < 256; k++) {
       runner += frequency[k];
@@ -73,6 +75,8 @@ int main(int argc, char *argv[]) {
         break;
       }
     }
+    
+    // append character to output var
     int len = strlen(msg);
     msg[len] = character;
     msg[len+1] = '\0';
